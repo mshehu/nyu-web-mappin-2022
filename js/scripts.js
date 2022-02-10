ee68baaa4a977687f44ad84522f613939a5f8ff7
mshehu <mateosshehu@gmail.com>
2022-02-10 17:01:43 -0500

    updated with refactored code

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,11 @@
 // there are buttons clicked
-
-$('#albania').on('click', function() {
-  $("#main").hide();
-  $('#rank1').show();
+$('.country-container').on('click', function() {
+  $('#main').hide();
+  var sectionId = $(this).attr('section-id');
+  $('#' + sectionId).show();
 })
 
-$('#italy').on('click', function() {
-  $("#main").hide();
-  $('#rank2').show();
-})
-
-$('#greece').on('click', function() {
-  $("#main").hide();
-  $('#rank3').show();
-})
-
-$('#rank1_goback').on('click', function() {
-  $('#rank1').hide();
-  $("#main").show();
-})
-
-$('#rank2_goback').on('click', function() {
-  $('#rank2').hide();
-  $("#main").show();
-})
-
-$('#rank3_goback').on('click', function() {
-  $('#rank3').hide();
-  $("#main").show();
+$('.btn-go-back').on('click', function() {
+  $(this).closest('.full-width').hide();
+  $('#main').show();
 })
